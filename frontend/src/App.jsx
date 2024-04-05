4c5d0621d7438c571e7665df7e244a5bd3631336
abhijeettrivedi1 <abhijeettrivedi3064@gmail.com>
2024-04-05 13:12:53 +0530

    lol

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,23 @@
 import { useState } from 'react'
 import './App.css'
-import { Route,Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
-
+import IndexPage from './pages/IndexPage'
+import { BrowserRouter } from 'react-router-dom';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout/>}>
+        <Route path='/' element={<Layout />}>
           <Route index element={<IndexPage />} />
-          <Route path="/login" element={<LoginPage/>} />
-          <Route path="/register" element={<RegisterPage/>} />
-          
-         </Route>
-        
+        </Route>
+
       </Routes>
-    </>
+
+    </BrowserRouter>
+
+
   )
 }
 
--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -1,8 +1,26 @@
-import { Link } from "react-router-dom";
-export default function Header(){
-      return(
-        <>
-        
-        </>
-      );
-  }
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header-content">
+          <a href="/" className="header-logo">Adelfox</a>
+          <nav className="header-nav">
+            <ul>
+              <li><a href="/product">Product</a></li>
+              <li><a href="/community">Community</a></li>
+              <li><a href="/plans">Plans</a></li>
+            </ul>
+          </nav>
+          <div className="header-actions">
+            <a href="/login" className="header-login">Login</a>
+            <a href="/signup" className="header-signup">Sign Up</a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
